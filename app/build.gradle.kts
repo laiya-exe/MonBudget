017d1b303d2ed8b38f05575494c4858a90d432cf
laiya-exe <akinlaiyaakponikpe@gmail.com>
2026-05-10 16:18:22 +0000

    correction barre de nav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,9 +60,6 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:$navVersion")
     implementation("androidx.navigation:navigation-ui:$navVersion")
 
-    // Material Design
-    implementation("com.google.android.material:material:1.12.0")
-
     // RecyclerView et CardView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
