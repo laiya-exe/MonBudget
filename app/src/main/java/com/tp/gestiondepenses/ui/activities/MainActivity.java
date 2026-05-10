38b49ac0a861f378cd3aff97631f62212d3a8508
laiya-exe <akinlaiyaakponikpe@gmail.com>
2026-05-10 17:45:57 +0000

    ajout barre transversale

--- app/src/main/java/com/tp/gestiondepenses/ui/activities/MainActivity.java
+++ app/src/main/java/com/tp/gestiondepenses/ui/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.tp.gestiondepenses.ui.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
+import androidx.core.view.WindowCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.tp.gestiondepenses.R;
@@ -19,16 +21,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
 
         setContentView(R.layout.activity_main);
 
-        // Récupérer le NavController depuis le NavHostFragment
+        // IMPORTANT : corrige la top bar
+        View topBar = findViewById(R.id.layout_top_bar);
+
+        ViewCompat.setOnApplyWindowInsetsListener(topBar, (view, insets) -> {
+
+            int statusBarHeight = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top;
+
+            view.setPadding(
+                    view.getPaddingLeft(),
+                    statusBarHeight,
+                    view.getPaddingRight(),
+                    view.getPaddingBottom()
+            );
+
+            return insets;
+        });
+
+        // NavController
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
+
         NavController navController = navHostFragment.getNavController();
 
-        // Lier le BottomNavigationView au NavController
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         NavigationUI.setupWithNavController(bottomNav, navController);
     }
